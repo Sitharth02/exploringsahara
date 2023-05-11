e29d08ab6c96d9a696c601850b9b19b97cb80639
Sitharth02 <120878429+Sitharth02@users.noreply.github.com>
2023-05-11 13:49:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get the form element
+const form = document.querySelector('form');
+
+// Add a submit event listener to the form
+form.addEventListener('submit', (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Display a confirmation message
+  alert('Thank you for contacting us!');
+});
